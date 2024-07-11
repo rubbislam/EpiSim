5cb9a46d1488523ebe9315cf66595a09277ffb32
Qianying Lin <qianying@lanl.gov>
2024-07-11 17:45:59 -0600

    update Lec 3

--- ref materials qmd/pfilter/model_measSIR.R
+++ ref materials qmd/pfilter/model_measSIR.R
@@ -2,9 +2,9 @@
 library(tidyverse)
 library(pomp)
 
-sir_step <- Csnippet("
+sir_stoch <- Csnippet("
   double dN_SI = rbinom(S,1-exp(-Beta*I/N*dt));
-  double dN_IR = rbinom(I,1-exp(-mu_IR*dt));
+  double dN_IR = rbinom(I,1-exp(-Gamma*dt));
   S -= dN_SI;
   I += dN_SI - dN_IR;
   R += dN_IR;
@@ -12,22 +12,22 @@ sir_step <- Csnippet("
 )
 
 sir_init <- Csnippet("
-  S = nearbyint(eta*N);
+  S = nearbyint(Eta*N);
   I = 1;
-  R = nearbyint((1-eta)*N);
+  R = nearbyint((1-Eta)*N);
   H = 0;"
 )
 
 dmeas <- Csnippet("
-  lik = dnbinom_mu(reports,k,rho*H,give_log);"
+  lik = dnbinom_mu(reports,k,Rho*H,give_log);"
 )
 
 rmeas <- Csnippet("
-  reports = rnbinom_mu(k,rho*H);"
+  reports = rnbinom_mu(k,Rho*H);"
 )
 
 emeas <- Csnippet("
-  E_reports = rho*H;"
+  E_reports = Rho*H;"
 )
 
 read_csv("https://kingaa.github.io/sbied/pfilter/Measles_Consett_1948.csv") |>
@@ -35,13 +35,13 @@ read_csv("https://kingaa.github.io/sbied/pfilter/Measles_Consett_1948.csv") |>
   filter(week<=42) |>
   pomp(
     times="week",t0=0,
-    rprocess=euler(sir_step,delta.t=1/7),
+    rprocess=euler(sir_stoch,delta.t=1/7),
     rinit=sir_init,
     rmeasure=rmeas,
     dmeasure=dmeas,
     emeasure=emeas,
     accumvars="H",
     statenames=c("S","I","R","H"),
-    paramnames=c("Beta","mu_IR","eta","rho","k","N"),
-    params=c(Beta=15,mu_IR=0.5,rho=0.5,k=10,eta=0.06,N=38000)
+    paramnames=c("Beta","Gamma","Eta","Rho","k","N"),
+    params=c(Beta=15,Gamma=0.5,Rho=0.5,k=10,Eta=0.06,N=38000)
   ) -> measSIR
