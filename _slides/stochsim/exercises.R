531b514aab6da519e6944b235a5b0568d1599806
Qianying Lin <qianying@lanl.gov>
2024-07-25 14:24:24 -0600

    update htmls

--- _slides/stochsim/exercises.R
+++ _slides/stochsim/exercises.R
@@ -1,40 +1,52 @@
 params <-
 list(prefix = "exercises")
 
+## ------------------------------------------------------------------------------------------------------------------
+#| include: false
 source("main.R")
 
+
+## ------------------------------------------------------------------------------------------------------------------
 measSIR |>
-  simulate(params=c(Beta=25,mu_IR=0.5,rho=0.5,k=10,eta=0.03,N=38000),
+  simulate(params=c(Beta=25,Gamma=0.5,Rho=0.5,k=10,Eta=0.03,N=38000),
+    nsim=20,format="data.frame",include.data=TRUE) |>
+  ggplot(aes(x=week,y=reports,group=.id,color=.id=="data"))+
+  geom_line() +
+  guides(color="none")
+
+
+## ------------------------------------------------------------------------------------------------------------------
+measSIR |>
+  simulate(params=c(Beta=40,Gamma=0.5,Rho=0.5,k=10,Eta=0.03,N=38000),
     nsim=20,format="data.frame",include.data=TRUE) |>
   ggplot(aes(x=week,y=reports,group=.id,color=.id=="data"))+
   geom_line()+
   guides(color="none")
 
+
+## ------------------------------------------------------------------------------------------------------------------
 measSIR |>
-  simulate(params=c(Beta=40,mu_IR=0.5,rho=0.5,k=10,eta=0.03,N=38000),
+  simulate(params=c(Beta=40,Gamma=0.2,Rho=0.5,k=10,Eta=0.03,N=38000),
     nsim=20,format="data.frame",include.data=TRUE) |>
   ggplot(aes(x=week,y=reports,group=.id,color=.id=="data"))+
   geom_line()+
   guides(color="none")
 
+
+## ------------------------------------------------------------------------------------------------------------------
 measSIR |>
-  simulate(params=c(Beta=40,mu_IR=0.2,rho=0.5,k=10,eta=0.03,N=38000),
+  simulate(params=c(Beta=15,Gamma=0.5,Rho=0.5,k=10,Eta=0.06,N=38000),
     nsim=20,format="data.frame",include.data=TRUE) |>
   ggplot(aes(x=week,y=reports,group=.id,color=.id=="data"))+
   geom_line()+
   guides(color="none")
 
-measSIR |>
-  simulate(params=c(Beta=15,mu_IR=0.5,rho=0.5,k=10,eta=0.06,N=38000),
-    nsim=20,format="data.frame",include.data=TRUE) |>
-  ggplot(aes(x=week,y=reports,group=.id,color=.id=="data"))+
-  geom_line()+
-  guides(color="none")
 
+## ------------------------------------------------------------------------------------------------------------------
 seir_step <- Csnippet("
   double dN_SE = rbinom(S,1-exp(-Beta*I/N*dt));
-  double dN_EI = rbinom(E,1-exp(-mu_EI*dt));
-  double dN_IR = rbinom(I,1-exp(-mu_IR*dt));
+  double dN_EI = rbinom(E,1-exp(-Sigma*dt));
+  double dN_IR = rbinom(I,1-exp(-Gamma*dt));
   S -= dN_SE;
   E += dN_SE - dN_EI;
   I += dN_EI - dN_IR;
@@ -44,10 +56,10 @@ seir_step <- Csnippet("
 
 
 seir_init <- Csnippet("
-  S = nearbyint(eta*N);
+  S = nearbyint(Eta*N);
   E = 0;
   I = 1;
-  R = nearbyint((1-eta)*N);
+  R = nearbyint((1-Eta)*N);
   H = 0;
 ")
 
@@ -55,20 +67,25 @@ measSIR |>
   pomp(
     rprocess=euler(seir_step,delta.t=1/7),
     rinit=seir_init,
-    paramnames=c("N","Beta","mu_EI","mu_IR","rho","eta"),
+    paramnames=c("N","Beta","Sigma","Gamma","Rho","Eta"),
     statenames=c("S","E","I","R","H")
   ) -> measSEIR
 
+
+## ------------------------------------------------------------------------------------------------------------------
 measSEIR |>
-  simulate(params=c(Beta=30,mu_EI=0.8,mu_IR=1.3,rho=0.5,k=10,eta=0.06,N=38000),
+  simulate(params=c(Beta=30,Sigma=0.8,Gamma=1.3,Rho=0.5,k=10,Eta=0.06,N=38000),
     nsim=20,format="data.frame",include.data=TRUE) |>
   ggplot(aes(x=week,y=reports,group=.id,color=.id=="data"))+
   geom_line()+
   guides(color="none")
 
+
+## ------------------------------------------------------------------------------------------------------------------
 measSEIR |> 
-  simulate(params=c(Beta=40,mu_EI=0.8,mu_IR=1.3,rho=0.5,k=10,eta=0.06,N=38000),
+  simulate(params=c(Beta=40,Sigma=0.8,Gamma=1.3,Rho=0.5,k=10,Eta=0.06,N=38000),
   nsim=20,format="data.frame",include.data=TRUE) |>
   ggplot(aes(x=week,y=reports,group=.id,color=.id=="data"))+
   geom_line()+
   guides(color="none")
+
--- _slides/stochsim/seir3_exercise.R
+++ _slides/stochsim/seir3_exercise.R
@@ -1,6 +1,7 @@
 params <-
 list(prefix = "seir3_exercise")
 
+## ----prelims,include=FALSE,purl=TRUE,cache=FALSE-------------------------------------------------------------------
 library(tidyverse)
 library(pomp)
 theme_set(theme_bw())
@@ -9,6 +10,8 @@ set.seed(1221234211)
 
 
 
+
+## ----seir3_model---------------------------------------------------------------------------------------------------
 library(tidyverse)
 library(pomp)
 
@@ -18,10 +21,10 @@ bsflu |>
     rprocess=euler(Csnippet("
       double N = 763;
       double t1 = rbinom(S,1-exp(-Beta*I/N*dt));
-      double t2 = rbinom(E,1-exp(-mu_E*dt));
-      double t3 = rbinom(I,1-exp(-mu_I*dt));
-      double t4 = rbinom(R1,1-exp(-mu_R1*dt));
-      double t5 = rbinom(R2,1-exp(-mu_R2*dt));
+      double t2 = rbinom(E,1-exp(-Mu_E*dt));
+      double t3 = rbinom(I,1-exp(-Mu_I*dt));
+      double t4 = rbinom(R1,1-exp(-Mu_R1*dt));
+      double t5 = rbinom(R2,1-exp(-Mu_R2*dt));
       S  -= t1;
       E  += t1 - t2;
       I  += t2 - t3;
@@ -35,16 +38,20 @@ bsflu |>
       R1 = 0;
       R2 = 0;"),
     dmeasure=Csnippet("
-      lik = dpois(B,rho*R1+1e-6,give_log);"),
+      lik = dpois(B,Rho*R1+1e-6,give_log);"),
     rmeasure=Csnippet("
-      B = rpois(rho*R1+1e-6);"),
+      B = rpois(Rho*R1+1e-6);"),
     statenames=c("S","E","I","R1","R2"),
-    paramnames=c("Beta","mu_E","mu_I","mu_R1","mu_R2","rho")
+    paramnames=c("Beta","Mu_E","Mu_I","Mu_R1","Mu_R2","Rho")
   ) -> flu
 
-with(bsflu,c(mu_R1=1/(sum(B)/512),mu_R2=1/(sum(C)/512)))
 
-coef(flu) <- c(Beta=5,mu_E=0.5,mu_I=1,mu_R1=0.33,mu_R2=0.55,rho=0.95)
+## ----fixedparams---------------------------------------------------------------------------------------------------
+with(bsflu,c(Mu_R1=1/(sum(B)/512),Mu_R2=1/(sum(C)/512)))
+
+
+## ----simulations---------------------------------------------------------------------------------------------------
+coef(flu) <- c(Beta=5,Mu_E=0.5,Mu_I=1,Mu_R1=0.33,Mu_R2=0.55,Rho=0.95)
 
 flu |>
   simulate(nsim=20,format="data.frame",include.data=TRUE) -> simdat
@@ -56,3 +63,4 @@ simdat |>
   scale_color_manual(values=c(`TRUE`="red",`FALSE`="black"))+
   scale_size_manual(values=c(`TRUE`=1,`FALSE`=0.5))+
   guides(color="none",size="none")
+
