bf47a49c1292ab4ca010972b932a9e5a78580447
Zian Zhuang <larry.zhuangzian@gmail.com>
2024-07-24 23:59:27 -0700

    update r script

--- _slides/mif/main.R
+++ _slides/mif/main.R
@@ -1,3 +1,592 @@
-## --------------------------------------------------------------------------------------------------------------------------------------
-source("../_includes/setup.R", local = knitr::knit_global())
+## ----prelims,echo=F,cache=F---------------------------------------------------------------
+library(foreach)
+library(iterators)
+library(doFuture)
+library(tidyverse)
+library(pomp)
+set.seed(1350254336)
+
+
+## ----model-construct----------------------------------------------------------------------
+source("model_measSIR.R")
+
+
+
+
+## ----fixed_params-------------------------------------------------------------------------
+fixed_params <- c(N=38000, Gamma=2, k=10)
+coef(measSIR,names(fixed_params)) <- fixed_params
+coef(measSIR)
+
+
+## ----sanity, echo=FALSE, fig.width=6, fig.height=4, dpi=300-------------------------------
+#| out-height: 70%
+measSIR |>
+  simulate(nsim=20,format="data.frame",include.data=TRUE) |>
+  ggplot(aes(x=week,y=reports,group=.id,color=.id=="data")) +
+  geom_line() + guides(color="none")
+
+
+## ----parallel-setup,cache=FALSE-----------------------------------------------------------
+library(foreach)
+library(doFuture)
+plan(multisession)
+
+
+
+
+## ----pf,echo=FALSE------------------------------------------------------------------------
+tic <- Sys.time()
+foreach(i=1:10,.combine=c,
+  .options.future=list(seed=TRUE)) %dofuture% {
+  measSIR |> pfilter(Np=5000)
+} -> pf
+pf |> logLik() |> logmeanexp(se=TRUE) -> L_pf
+L_pf
+toc <- Sys.time()
+
+
+## ----init_csv,cache=FALSE-----------------------------------------------------------------
+pf[[1]] |> coef() |> bind_rows() |>
+  bind_cols(loglik=L_pf[1],loglik.se=L_pf[2]) |>
+  write_csv("measles_params.csv")
+
+
+
+
+## ----local_search_eval,echo=FALSE---------------------------------------------------------
+## What is this 'bake' function?
+## See https://kingaa.github.io/sbied/pfilter/bake.html
+## for an explanation.
+bake(file="local_search.rds",{
+  foreach(i=1:20,.combine=c,
+    .options.future=list(seed=482947940)
+  ) %dofuture% {
+    measSIR |>
+      mif2(
+        Np=2000, Nmif=50, cooling.fraction.50=0.5,
+        rw.sd=rw_sd(Beta=0.02, Rho=0.02, Eta=ivp(0.02)),
+        partrans=parameter_trans(
+          log="Beta",logit=c("Rho","Eta")
+        ),
+        paramnames=c("Beta","Rho","Eta")
+      )
+  } -> mifs_local
+  attr(mifs_local,"ncpu") <- nbrOfWorkers()
+  mifs_local
+}) -> mifs_local
+t_loc <- attr(mifs_local,"system.time")
+ncpu_loc <- attr(mifs_local,"ncpu")
+
+
+## ----local_search_plot,fig.width=6, fig.height=4, dpi=300---------------------------------
+#| out-height: 80%
+mifs_local |>
+  traces() |>
+  melt() |>
+  ggplot(aes(x=iteration,y=value,group=.L1,color=factor(.L1)))+
+  geom_line()+
+  guides(color="none")+
+  facet_wrap(~name,scales="free_y")
+
+
+
+
+## ----lik_local_eval,include=FALSE---------------------------------------------------------
+bake(file="lik_local.rds",{
+  foreach(mf=mifs_local,.combine=rbind,
+    .options.future=list(seed=900242057)
+  ) %dofuture% {
+    evals <- replicate(10, logLik(pfilter(mf,Np=5000)))
+    ll <- logmeanexp(evals,se=TRUE)
+    mf |> coef() |> bind_rows() |>
+      bind_cols(loglik=ll[1],loglik.se=ll[2])
+  } -> results
+  attr(results,"ncpu") <- nbrOfWorkers()
+  results
+}) -> results
+t_local <- attr(results,"system.time")
+ncpu_local <- attr(results,"ncpu")
+
+
+## ----mle_local----------------------------------------------------------------------------
+results |> filter(loglik==max(loglik))
+
+
+## ----pairs_local,fig.width=8, fig.height=6, dpi=300---------------------------------------
+#| out-height: 80%
+pairs(~loglik+Beta+Eta+Rho,data=results,pch=16)
+
+
+## ----local_database,cache=FALSE-----------------------------------------------------------
+read_csv("measles_params.csv") |>
+  bind_rows(results) |>
+  arrange(-loglik) |>
+  write_csv("measles_params.csv")
+
+
+## ----cluster_setup,include=FALSE,purl=TRUE------------------------------------------------
+if (file.exists("CLUSTER.R")) {
+  source("CLUSTER.R")
+}
+
+
+## ----global_search1-----------------------------------------------------------------------
+set.seed(2062379496)
+
+runif_design(
+  lower=c(Beta=5,Rho=0.2,Eta=0),
+  upper=c(Beta=80,Rho=0.9,Eta=1),
+  nseq=400
+) -> guesses
+
+mf1 <- mifs_local[[1]]
+
+
+
+
+## ----global_search_eval,include=FALSE-----------------------------------------------------
+bake(file="global_search.rds",
+  dependson=guesses,{
+    foreach(guess=iter(guesses,"row"), .combine=rbind,
+      .options.future=list(seed=1270401374)
+    ) %dofuture% {
+      mf1 |>
+        mif2(params=c(guess,fixed_params)) |>
+        mif2(Nmif=100) -> mf
+      replicate(
+        10,
+        mf |> pfilter(Np=5000) |> logLik()
+      ) |>
+        logmeanexp(se=TRUE) -> ll
+      mf |> coef() |> bind_rows() |>
+        bind_cols(loglik=ll[1],loglik.se=ll[2])
+    } -> results
+    attr(results,"ncpu") <- nbrOfWorkers()
+    results
+  }) |>
+  filter(is.finite(loglik)) -> results
+t_global <- attr(results,"system.time")
+ncpu_global <- attr(results,"ncpu")
+
+
+## ----cache=FALSE,include=FALSE------------------------------------------------------------
+read_csv("measles_params.csv") |>
+  bind_rows(results) |>
+  filter(is.finite(loglik)) |>
+  arrange(-loglik) |>
+  write_csv("measles_params.csv")
+
+
+## ----mle_global---------------------------------------------------------------------------
+results |> filter(loglik==max(loglik))
+
+
+## ----pairs_global1,fig.width=8, fig.height=6, dpi=300-------------------------------------
+#| out-height: 80%
+read_csv("measles_params.csv") |>
+  filter(loglik>max(loglik)-50) |>
+  bind_rows(guesses) |>
+  mutate(type=if_else(is.na(loglik),"guess","result")) |>
+  arrange(type) -> all
+
+pairs(~loglik+Beta+Eta+Rho, data=all, pch=16, cex=0.3,
+  col=ifelse(all$type=="guess",grey(0.5),"red"))
+
+
+## ----pairs_global2, fig.width=8, fig.height=6, dpi=300------------------------------------
+#| out-height: 80%
+all |>
+  filter(type=="result") |>
+  filter(loglik>max(loglik)-10) |>
+  ggplot(aes(x=Eta,y=loglik))+
+  geom_point()+
+  labs(
+    x=expression(Eta),
+    title="poor man's profile likelihood"
+  )
+
+
+## ----eta_profile1a------------------------------------------------------------------------
+read_csv("measles_params.csv") |>
+  filter(loglik>max(loglik)-20,loglik.se<2) |>
+  sapply(range) -> box
+box
+
+
+## ----eta_profile1b, fig.width=6, fig.height=4, dpi=300------------------------------------
+#| out-height: 80%
+freeze(seed=1196696958,
+  profile_design(
+    Eta=seq(0.01,0.95,length=40),
+    lower=box[1,c("Beta","Rho")],
+    upper=box[2,c("Beta","Rho")],
+    nprof=15, type="runif"
+  )) -> guesses
+plot(guesses)
+
+
+
+
+## ----eta_profile2_eval,include=FALSE------------------------------------------------------
+mf1 <- mifs_local[[1]]
+bake(file="eta_profile.rds",
+  dependson=guesses,{
+    foreach(guess=iter(guesses,"row"), .combine=rbind,
+      .options.future=list(seed=830007657)
+    ) %dofuture% {
+      mf1 |>
+        mif2(params=c(guess,fixed_params),
+          rw.sd=rw_sd(Beta=0.02,Rho=0.02)) |>
+        mif2(Nmif=100,cooling.fraction.50=0.3) -> mf
+      replicate(
+        10,
+        mf |> pfilter(Np=5000) |> logLik()) |>
+        logmeanexp(se=TRUE) -> ll
+      mf |> coef() |> bind_rows() |>
+        bind_cols(loglik=ll[1],loglik.se=ll[2])
+    } -> results
+    attr(results,"ncpu") <- nbrOfWorkers()
+    results
+  }) -> results
+t_eta <- attr(results,"system.time")
+ncpu_eta <- attr(results,"ncpu")
+
+
+## ----eta_profile_database,cache=FALSE-----------------------------------------------------
+read_csv("measles_params.csv") |>
+  bind_rows(results) |>
+  filter(is.finite(loglik)) |>
+  arrange(-loglik) |>
+  write_csv("measles_params.csv")
+
+
+## ----eta_profile_pairs, fig.width=8, fig.height=6, dpi=300--------------------------------
+#| out-height: 80%
+read_csv("measles_params.csv") |>
+  filter(loglik>max(loglik)-10) -> all
+
+pairs(~loglik+Beta+Eta+Rho,data=all,pch=16)
+
+
+## ----eta_profile_plot1, fig.width=6, fig.height=4, dpi=300--------------------------------
+#| out-height: 80%
+results |>
+  ggplot(aes(x=Eta,y=loglik))+
+  geom_point() + xlab(expression(eta))
+
+
+## ----eta_profile_plot2, fig.width=6, fig.height=4, dpi=300--------------------------------
+#| out-height: 80%
+results |>
+  filter(is.finite(loglik)) |>
+  group_by(round(Eta,5)) |>
+  filter(rank(-loglik)<3) |>
+  ungroup() |>
+  filter(loglik>max(loglik)-20) |>
+  ggplot(aes(x=Eta,y=loglik))+
+  geom_point() + xlab(expression(eta))
+
+
+## ----eta_profile_plot3, fig.width=6, fig.height=4, dpi=300--------------------------------
+#| out-height: 80%
+maxloglik <- max(results$loglik,na.rm=TRUE)
+ci.cutoff <- maxloglik-0.5*qchisq(df=1,p=0.95)
+
+results |>
+  filter(is.finite(loglik)) |>
+  group_by(round(Eta,5)) |>
+  filter(rank(-loglik)<3) |>
+  ungroup() |>
+  ggplot(aes(x=Eta,y=loglik))+
+  geom_point() + xlab(expression(eta)) +
+  geom_smooth(method="loess",span=0.25)+
+  geom_hline(color="red",yintercept=ci.cutoff)+
+  lims(y=maxloglik-c(5,0))
+
+
+## ----eta_profile_eta_by_rho, fig.width=6, fig.height=4, dpi=300---------------------------
+#| out-height: 80%
+results |>
+  filter(is.finite(loglik)) |>
+  group_by(round(Eta,5)) |>
+  filter(rank(-loglik)<3) |>
+  ungroup() |>
+  mutate(in_ci=loglik>max(loglik)-1.92) |>
+  ggplot(aes(x=Eta,y=Rho,color=in_ci))+
+  geom_point()+
+  labs(
+    color="inside 95% CI?",
+    x=expression(eta),
+    y=expression(rho),
+    title="profile trace"
+  )
+
+
+## ----include=FALSE------------------------------------------------------------------------
+results |>
+  filter(is.finite(loglik)) |>
+  filter(loglik>max(loglik)-0.5*qchisq(df=1,p=0.95)) |>
+  summarize(min=min(Rho),max=max(Rho)) -> rho_ci
+
+
+## ----rho_profile1-------------------------------------------------------------------------
+read_csv("measles_params.csv") |>
+  group_by(cut=round(Rho,2)) |>
+  filter(rank(-loglik)<=10) |>
+  ungroup() |>
+  arrange(-loglik) |>
+  select(-cut,-loglik,-loglik.se) -> guesses
+
+
+
+
+## ----rho_profile_eval,include=FALSE-------------------------------------------------------
+mf1 <- mifs_local[[1]]
+bake(file="rho_profile.rds",
+  dependson=guesses,{
+    foreach(guess=iter(guesses,"row"), .combine=rbind,
+      .options.future=list(seed=2105684752)
+    ) %dofuture% {
+      mf1 |>
+        mif2(params=guess,
+          rw.sd=rw_sd(Beta=0.02,Eta=ivp(0.02))) |>
+        mif2(Nmif=100,cooling.fraction.50=0.3) |>
+        mif2() -> mf
+      replicate(
+        10,
+        mf |> pfilter(Np=5000) |> logLik()) |>
+        logmeanexp(se=TRUE) -> ll
+      mf |> coef() |> bind_rows() |>
+        bind_cols(loglik=ll[1],loglik.se=ll[2])
+    } -> results
+    attr(results,"ncpu") <- nbrOfWorkers()
+    results
+  }) -> results
+t_rho <- attr(results,"system.time")
+ncpu_rho <- attr(results,"ncpu")
+
+
+## ----include=FALSE,cache=FALSE------------------------------------------------------------
+read_csv("measles_params.csv") |>
+  bind_rows(results) |>
+  filter(is.finite(loglik)) |>
+  arrange(-loglik) |>
+  write_csv("measles_params.csv")
+
+
+## ----prof-rho1, echo=FALSE, fig.width=8, fig.height=6, dpi=300----------------------------
+#| out-height: 80%
+results |> filter(is.finite(loglik)) -> results
+
+pairs(~loglik+Beta+Eta+Rho,data=results,pch=16)
+
+
+## ----echo=FALSE, fig.width=6, fig.height=4, dpi=300---------------------------------------
+#| out-height: 80%
+results |>
+  filter(loglik>max(loglik)-10,loglik.se<1) |>
+  group_by(round(Rho,2)) |>
+  filter(rank(-loglik)<3) |>
+  ungroup() |>
+  ggplot(aes(x=Rho,y=loglik))+
+  geom_point() + xlab(expression(rho)) +
+  geom_hline(
+    color="red",
+    yintercept=max(results$loglik)-0.5*qchisq(df=1,p=0.95)
+  )
+
+
+## ----rho_ci-------------------------------------------------------------------------------
+results |>
+  filter(loglik>max(loglik)-0.5*qchisq(df=1,p=0.95)) |>
+  summarize(min=min(Rho),max=max(Rho)) -> rho_ci
+
+
+## ----exp_global_search1-------------------------------------------------------------------
+freeze(seed=55266255,
+  runif_design(
+    lower=c(Beta=5,Gamma=0.2,Eta=0),
+    upper=c(Beta=80,Gamma=5,Eta=0.99),
+    nseq=1000
+  )) |>
+  mutate(
+    Rho=0.6, k=10, N=38000
+  ) -> guesses
+
+
+
+
+
+
+## ----exp_global_search_eval,include=FALSE-------------------------------------------------
+bake(file="global_search2.rds",
+  dependson=guesses,{
+    foreach(
+      guess=iter(guesses,"row"), .combine=rbind,
+      .options.future=list(seed=610408798)
+    ) %dofuture% {
+      measSIR |>
+        mif2(params=guess, Np=2000, Nmif=100,
+          cooling.fraction.50=0.5,
+          partrans=parameter_trans(
+            log=c("Beta","Gamma"),
+            logit="Eta"), paramnames=c("Beta","Gamma","Eta"),
+          rw.sd=rw_sd(Beta=0.02,Gamma=0.02,Eta=ivp(0.02))) -> mf
+      mf |>
+        mif2(
+          Nmif=100,rw.sd=rw_sd(Beta=0.01,Gamma=0.01,Eta=ivp(0.01))
+        ) |>
+        mif2(
+          Nmif=100,
+          rw.sd=rw_sd(Beta=0.005,Gamma=0.005,Eta=ivp(0.005))
+        ) -> mf
+      replicate(
+        10,
+        mf |> pfilter(Np=5000) |> logLik()
+      ) |> logmeanexp(se=TRUE) -> ll
+      mf |> coef() |> bind_rows() |>
+        bind_cols(loglik=ll[1],loglik.se=ll[2])
+    } -> results
+    attr(results,"ncpu") <- nbrOfWorkers()
+    results
+  }) |>
+  filter(is.finite(loglik)) -> results
+t_expglob <- attr(results,"system.time")
+ncpu_expglob <- attr(results,"ncpu")
+
+
+## ----include=FALSE,cache=FALSE------------------------------------------------------------
+read_csv("measles_params.csv") |>
+  bind_rows(results) |>
+  filter(is.finite(loglik)) |>
+  arrange(-loglik) |>
+  write_csv("measles_params.csv")
+
+
+## ----another-search, fig.width=8, fig.height=6, dpi=300-----------------------------------
+#| out-height: 80%
+read_csv("measles_params.csv") |>
+  filter(loglik>max(loglik)-20) -> all
+
+pairs(~loglik+Rho+Gamma+Beta+Eta,data=all,pch=16,cex=0.3,
+  col=if_else(round(all$Rho,3)==0.6,1,4))
+
+
+## ----poorman-gamma2, fig.width=8, fig.height=6, dpi=300-----------------------------------
+#| out-height: 80%
+results |>
+  filter(loglik>max(loglik)-20,loglik.se<1) |>
+  ggplot(aes(x=Gamma,y=loglik))+
+  geom_point()+
+  geom_hline(
+    color="red",
+    yintercept=max(results$loglik)-0.5*qchisq(df=1,p=0.95)
+  )
+
+
+## ----gamma_profile1a----------------------------------------------------------------------
+read_csv("measles_params.csv") |>
+  filter(
+    loglik>max(loglik)-20,
+    loglik.se<2,
+    abs(Rho-0.6)<0.01
+  ) |>
+  sapply(range) -> box
+
+
+## ----gamma_profile1b----------------------------------------------------------------------
+freeze(seed=610408798,
+  profile_design(
+    Gamma=seq(0.2,2,by=0.1),
+    lower=box[1,c("Beta","Eta")],
+    upper=box[2,c("Beta","Eta")],
+    nprof=100, type="runif"
+  )) |>
+  mutate(
+    N=38000,
+    Rho=0.6,
+    k=10
+  ) -> guesses
+
+
+
+
+## ----gamma_profile_eval,include=FALSE-----------------------------------------------------
+bake(file="gamma_profile1.rds",
+  dependson=guesses,{
+    foreach(guess=iter(guesses,"row"), .combine=rbind,
+      .options.future=list(seed=610408798)
+    ) %dofuture% {
+      measSIR |>
+        mif2(params=guess, Np=2000, Nmif=100,
+          partrans=parameter_trans(log="Beta",logit="Eta"),
+          paramnames=c("Beta","Eta"), cooling.fraction.50=0.5,
+          rw.sd=rw_sd(Beta=0.02,Eta=ivp(0.02))
+        ) |>
+        mif2(Nmif=100) |>
+        mif2(Nmif=100,rw.sd=rw_sd(Beta=0.01,Eta=ivp(0.01))) |>
+        mif2(Nmif=100,rw.sd=rw_sd(Beta=0.005,Eta=ivp(0.005))) -> mf
+      replicate(10,mf |> pfilter(Np=5000) |> logLik()) |>
+        logmeanexp(se=TRUE) -> ll
+      mf |> coef() |> bind_rows() |>
+        bind_cols(loglik=ll[1],loglik.se=ll[2])
+    } -> results
+    attr(results,"ncpu") <- nbrOfWorkers()
+    results
+  }) |>
+  filter(is.finite(loglik)) -> results
+t_muIR <- attr(results,"system.time")
+ncpu_muIR <- attr(results,"ncpu")
+
+
+## ----include=FALSE,cache=FALSE------------------------------------------------------------
+read_csv("measles_params.csv") |>
+  bind_rows(results) |>
+  filter(is.finite(loglik)) |>
+  arrange(-loglik) |>
+  write_csv("measles_params.csv")
+
+
+## ----gamma_profile_plot, fig.height=4, fig.width=6, dpi=300-------------------------------
+#| out-height: 80%
+results |>
+  group_by(round(Gamma,2)) |>
+  filter(rank(-loglik)<=1) |>
+  ungroup() |>
+  ggplot(aes(x=Gamma,y=loglik))+
+  geom_point() + xlab(expression(gamma)) +
+  geom_hline(
+    color="red",
+    yintercept=max(results$loglik)-0.5*qchisq(df=1,p=0.95)
+  )
+
+
+## -----------------------------------------------------------------------------------------
+read_csv("measles_params.csv") |>
+  filter(loglik == max(loglik)) |>
+  select(-loglik, -loglik.se) -> best.params
+
+measSIR |>
+  simulate(
+    params=unlist(best.params),
+    nsim=1000, format="data.frame", include.data=TRUE
+  ) -> sims
+
+
+## ----viz-predictions, fig.height=4, fig.width=6, dpi=300----------------------------------
+#| out-height: 80%
+sims |>
+  mutate(data=.id=="data") |>
+  group_by(week,data) |>
+  reframe(
+    p=c(0.025,0.5,0.975),
+    value=wquant(reports,probs=p),
+    name=c("lo","med","up")
+  ) |>
+  select(-p) |> pivot_wider() |> ungroup() |>
+  ggplot(aes(x=week,y=med,color=data,fill=data,ymin=lo,ymax=up))+
+  geom_line()+ geom_ribbon(alpha=0.2,color=NA) +
+  labs(y="reports")+
+  theme_bw() + guides(color="none",fill="none")
 
--- _slides/stochsim/main.R
+++ _slides/stochsim/main.R
@@ -1,44 +1,68 @@
+## ----doFuture,echo=FALSE,cache=FALSE------------------------------------------------------
 library(doFuture)
+library(microbenchmark)
 plan(multisession)
 set.seed(2488820)
 
-library(tidyverse)
-read_csv(paste0("https://kingaa.github.io/sbied/stochsim/",
-  "Measles_Consett_1948.csv")) |>
-  select(week,reports=cases) -> meas
-meas |> as.data.frame() |> head()
 
+## ----det-skel-----------------------------------------------------------------------------
+library(pomp)
+Csnippet("
+        DS = -Beta*S*I/N;
+        DI = Beta*S*I/N - I*Gamma;
+        DR = I*Gamma;
+        ") -> sir_det_skel
+
+
+## ----meas-data1---------------------------------------------------------------------------
+library(tidyverse)
+read_csv(paste0("https://kingaa.github.io/sbied/stochsim/", 
+  "Measles_Consett_1948.csv")) |> 
+  select(week,reports=cases) -> meas
+meas |> as.data.frame() |> head(n=3)
+
+
+## ----meas-data2,echo=FALSE,fig.width=6, fig.height=3--------------------------------------
 library(tidyverse)
 meas |>
-  ggplot(aes(x=week,y=reports))+
-  geom_line()+
+  ggplot(aes(x=week,y=reports)) +
+  geom_line() +
   geom_point()
 
-sir_step <- function (S, I, R, N, Beta, mu_IR, delta.t, ...)
-{
+
+## ----rproc1R------------------------------------------------------------------------------
+sir_stoch <- function (S, I, R, N, Beta, Gamma, delta.t, ...) {
   dN_SI <- rbinom(n=1,size=S,prob=1-exp(-Beta*I/N*delta.t))
-  dN_IR <- rbinom(n=1,size=I,prob=1-exp(-mu_IR*delta.t))
+  dN_IR <- rbinom(n=1,size=I,prob=1-exp(-Gamma*delta.t))
   S <- S - dN_SI
   I <- I + dN_SI - dN_IR
   R <- R + dN_IR
   c(S = S, I = I, R = R)
 }
 
-sir_rinit <- function (N, eta, ...) {
-  c(S = round(N*eta), I = 1, R = round(N*(1-eta)))
+
+
+
+## ----init1R-------------------------------------------------------------------------------
+sir_rinit <- function (N, Eta, ...) {
+  c(S = round(N*Eta), I = 1, R = round(N*(1-Eta)))
 }
 
+
+## ----pomp1R-------------------------------------------------------------------------------
 library(pomp)
 meas |>
-  pomp(times="week",t0=0,
-    rprocess=euler(sir_step,delta.t=1/7),
+  pomp(
+    times="week",t0=0,
+    rprocess=euler(sir_stoch,delta.t=1/7),
     rinit=sir_rinit
   ) -> measSIR
 
-sir_step <- function (S, I, R, N, Beta, mu_IR, delta.t,
-  H, ...) {
+
+## ----rproc2R------------------------------------------------------------------------------
+sir_stoch <- function (S, I, R, N, Beta, Gamma, delta.t, H, ...) {
   dN_SI <- rbinom(n=1,size=S,prob=1-exp(-Beta*I/N*delta.t))
-  dN_IR <- rbinom(n=1,size=I,prob=1-exp(-mu_IR*delta.t))
+  dN_IR <- rbinom(n=1,size=I,prob=1-exp(-Gamma*delta.t))
   S <- S - dN_SI
   I <- I + dN_SI - dN_IR
   R <- R + dN_IR
@@ -46,24 +70,31 @@ sir_step <- function (S, I, R, N, Beta, mu_IR, delta.t,
   c(S = S, I = I, R = R, H = H)
 }
 
-sir_rinit <- function (N, eta, ...) {
-  c(S = round(N*eta), I = 1, R = round(N*(1-eta)), H = 0)
+sir_rinit <- function (N, Eta, ...) {
+  c(S = round(N*Eta), I = 1, R = round(N*(1-Eta)), H = 0)
 }
 
+
+## ----zero1R-------------------------------------------------------------------------------
 measSIR |>
   pomp(
-    rprocess=euler(sir_step,delta.t=1/7),
-    rinit=sir_rinit, accumvars="H"
+    rprocess=euler(sir_stoch,delta.t=1/7),
+    rinit=sir_rinit, 
+    accumvars="H"
   ) -> measSIR
 
-sir_dmeas <- function (reports, H, rho, k, log, ...) {
-  dnbinom(x=reports, size=k, mu=rho*H, log=log)
+
+## ----meas-modelR--------------------------------------------------------------------------
+sir_dmeas <- function (reports, H, Rho, k, log, ...) {
+  dnbinom(x=reports, size=k, mu=Rho*H, log=log)
 }
 
-sir_rmeas <- function (H, rho, k, ...) {
-  c(reports=rnbinom(n=1, size=k, mu=rho*H))
+sir_rmeas <- function (H, Rho, k, ...) {
+  c(reports=rnbinom(n=1, size=k, mu=Rho*H))
 }
 
+
+## ----add-meas-modelR----------------------------------------------------------------------
 measSIR |>
   pomp(
     rmeasure=sir_rmeas,
@@ -71,49 +102,161 @@ measSIR |>
   ) -> measSIR
 
 
-
-sir_step <- Csnippet("
+## ----csnips-------------------------------------------------------------------------------
+sir_stoch <- Csnippet("
   double dN_SI = rbinom(S,1-exp(-Beta*I/N*dt));
-  double dN_IR = rbinom(I,1-exp(-mu_IR*dt));
+  double dN_IR = rbinom(I,1-exp(-Gamma*dt));
   S -= dN_SI;
   I += dN_SI - dN_IR;
   R += dN_IR;
   H += dN_IR;
 ")
 
-sir_rinit <- Csnippet("
-  S = nearbyint(eta*N);
-  I = 1;
-  R = nearbyint((1-eta)*N);
-  H = 0;
-")
 
-sir_dmeas <- Csnippet("
-  lik = dnbinom_mu(reports,k,rho*H,give_log);
-")
+## ----more-csnips--------------------------------------------------------------------------
+    sir_rinit <- Csnippet("
+      S = nearbyint(Eta*N);
+      I = 1;
+      R = nearbyint((1-Eta)*N);
+      H = 0;
+    ")
+    sir_dmeas <- Csnippet("
+      lik = dnbinom_mu(reports,k,Rho*H,give_log);
+    ")
+    sir_rmeas <- Csnippet("
+      reports = rnbinom_mu(k,Rho*H);
+    ")
 
-sir_rmeas <- Csnippet("
-  reports = rnbinom_mu(k,rho*H);
-")
 
+## ----sir_pomp-----------------------------------------------------------------------------
 measSIR |>
-  pomp(rprocess=euler(sir_step,delta.t=1/7),
+  pomp(
+    rprocess=euler(sir_stoch,delta.t=1/7),
     rinit=sir_rinit,
     rmeasure=sir_rmeas,
     dmeasure=sir_dmeas,
     accumvars="H",
     statenames=c("S","I","R","H"),
-    paramnames=c("Beta","mu_IR","N","eta","rho","k")
-  ) -> measSIR
+    paramnames=c("Beta","Gamma","N","Eta","Rho","k")
+  ) -> measSIR_C
 
+
+## ----CSnippet-compare, echo=FALSE, fig.width=4, fig.height=4, cache=TRUE------------------
+fR <- function() {measSIR |>
+      simulate(
+        params=c(Beta=7.5,Gamma=0.5,Rho=0.5,k=10,
+          Eta=0.03,N=38000),
+        nsim=100,format="data.frame",include.data=TRUE
+      )} 
+fC <- function() {measSIR_C |>
+      simulate(
+        params=c(Beta=7.5,Gamma=0.5,Rho=0.5,k=10,
+          Eta=0.03,N=38000),
+        nsim=100,format="data.frame",include.data=TRUE
+      )} 
+res <- microbenchmark(fR(), fC(), times=100L)
+
+ggplot(res, aes(x=expr, y=time/1000000, color=expr)) +
+  geom_boxplot(outliers = FALSE) +
+  scale_color_brewer(palette="Dark2") +
+  scale_x_discrete(labels = c("fR()" = "R", "fC()" = "CSnippet")) +
+  scale_y_log10() + 
+  labs(y = "time (milliseconds)", x = NULL) +
+  theme_bw() + guides(color="none")
+
+
+## ----sir_sims1, echo=FALSE----------------------------------------------------------------
 measSIR |>
   simulate(
-    params=c(Beta=7.5,mu_IR=0.5,rho=0.5,k=10,
-      eta=0.03,N=38000),
+    params=c(Beta=7.5,Gamma=0.5,Rho=0.5,k=10, Eta=0.03,N=38000),
     nsim=20,format="data.frame",include.data=TRUE
   ) -> sims
 
-sims |>
+
+## ----sir_sim1_plot,fig.width=8,fig.height=2-----------------------------------------------
+measSIR |> 
+  simulate(params=c(Beta=7.5,Gamma=0.5,Rho=0.5,k=10, Eta=0.03,N=38000),
+    nsim=20,format="data.frame",include.data=TRUE) |>
+  ggplot(aes(x=week,y=reports,group=.id,color=.id=="data")) +
+  geom_line() + guides(color="none")
+
+
+## ----fig.width=8, fig.height=1.5----------------------------------------------------------
+measSIR |>
+  simulate(params=c(Beta=25,Gamma=0.5,Rho=0.5,k=10,Eta=0.03,N=38000),
+    nsim=20,format="data.frame",include.data=TRUE) |>
+  ggplot(aes(x=week,y=reports,group=.id,color=.id=="data")) +
+  geom_line() + guides(color="none")
+
+
+## ----fig.width=8, fig.height=1.5----------------------------------------------------------
+measSIR |>
+  simulate(params=c(Beta=40,Gamma=0.5,Rho=0.5,k=10,Eta=0.03,N=38000),
+    nsim=20,format="data.frame",include.data=TRUE) |>
+  ggplot(aes(x=week,y=reports,group=.id,color=.id=="data")) +
+  geom_line() + guides(color="none")
+
+
+## ----fig.width=8, fig.height=1.5----------------------------------------------------------
+measSIR |>
+  simulate(params=c(Beta=40,Gamma=0.2,Rho=0.5,k=10,Eta=0.03,N=38000),
+    nsim=20,format="data.frame",include.data=TRUE) |>
   ggplot(aes(x=week,y=reports,group=.id,color=.id=="data"))+
-  geom_line()+
-  guides(color="none")
+  geom_line() + guides(color="none")
+
+
+## ----fig.width=8, fig.height=1.5----------------------------------------------------------
+measSIR |>
+  simulate(params=c(Beta=15,Gamma=0.5,Rho=0.5,k=10,Eta=0.06,N=38000),
+    nsim=20,format="data.frame",include.data=TRUE) |>
+  ggplot(aes(x=week,y=reports,group=.id,color=.id=="data")) +
+  geom_line() + guides(color="none")
+
+
+## -----------------------------------------------------------------------------------------
+seir_stoch <- Csnippet("
+  double dN_SE = rbinom(S,1-exp(-Beta*I/N*dt));
+  double dN_EI = rbinom(E,1-exp(-Sigma*dt));
+  double dN_IR = rbinom(I,1-exp(-Gamma*dt));
+  S -= dN_SE;
+  E += dN_SE - dN_EI;
+  I += dN_EI - dN_IR;
+  R += dN_IR;
+  H += dN_IR;
+")
+
+
+## -----------------------------------------------------------------------------------------
+seir_init <- Csnippet("
+  S = nearbyint(Eta*N);
+  E = 0; I = 1;
+  R = nearbyint((1-Eta)*N);
+  H = 0;
+")
+
+measSIR |>
+  pomp(
+    rprocess=euler(seir_stoch,delta.t=1/7),
+    rinit=seir_init,
+    paramnames=c("N","Beta","Sigma","Gamma","Rho","Eta","k"),
+    statenames=c("S","E","I","R","H")
+  ) -> measSEIR
+
+
+## ----fig.width=8, fig.height=1.3----------------------------------------------------------
+measSEIR |>
+  simulate(params=c(Beta=30,Sigma=0.8,Gamma=1.3,
+                    Rho=0.5,k=10,Eta=0.06,N=38000),
+    nsim=20,format="data.frame",include.data=TRUE) |>
+  ggplot(aes(x=week,y=reports,group=.id,color=.id=="data")) +
+  geom_line() + guides(color="none")
+
+
+## ----fig.width=8, fig.height=1.3----------------------------------------------------------
+measSEIR |> 
+  simulate(params=c(Beta=40,Sigma=0.8,Gamma=1.3,
+                    Rho=0.5,k=10,Eta=0.06,N=38000),
+    nsim=20,format="data.frame",include.data=TRUE) |>
+  ggplot(aes(x=week,y=reports,group=.id,color=.id=="data")) +
+  geom_line() + guides(color="none")
+
