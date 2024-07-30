21c7230644af2a3d3203c45055fb796bfe303753
Zian Zhuang <larry.zhuangzian@gmail.com>
2024-07-30 12:38:36 -0400

    update workflow

--- _slides/workflow/main.R
+++ _slides/workflow/main.R
@@ -1,4 +1,4 @@
-## ----prelims,echo=F,cache=F--------------------------------------------------------------------
+## ----prelims,echo=F,cache=F-----------------------------------------------------
 library(foreach)
 library(iterators)
 library(doFuture)
@@ -8,7 +8,7 @@ library(pomp)
 plan(multisession)
 
 
-## ----meas-data1--------------------------------------------------------------------------------
+## ----meas-data1-----------------------------------------------------------------
 library(tidyverse)
 read_csv("Measles_Consett_1948.csv") |>
   select(week,reports=cases) |>
@@ -16,13 +16,13 @@ read_csv("Measles_Consett_1948.csv") |>
 meas |> as.data.frame() |> head(n=3)
 
 
-## ----meas-data2, fig.width=5, fig.height=1.5, dpi=300------------------------------------------
+## ----meas-data2, fig.width=5, fig.height=1.5, dpi=300---------------------------
 meas |>
   ggplot(aes(x=week,y=reports)) +
   geom_line() + geom_point()
 
 
-## ----sir_pomp1---------------------------------------------------------------------------------
+## ----sir_pomp1------------------------------------------------------------------
 sir_stoch <- Csnippet("
   double dN_SI = rbinom(S,1-exp(-Beta*I/N*dt));
   double dN_IR = rbinom(I,1-exp(-Gamma*dt));
@@ -38,7 +38,7 @@ sir_init <- Csnippet("
 )
 
 
-## ----sir_pomp2---------------------------------------------------------------------------------
+## ----sir_pomp2------------------------------------------------------------------
 dmeas <- Csnippet("lik = dnbinom_mu(reports,k,Rho*H,give_log);")
 rmeas <- Csnippet("reports = rnbinom_mu(k,Rho*H);")
 
@@ -54,24 +54,24 @@ meas |>
   ) -> measSIR
 
 
-## ----fig.width=8, fig.height=2-----------------------------------------------------------------
+## ----fig.width=8, fig.height=2--------------------------------------------------
 measSIR |>
   simulate(nsim=20,format="data.frame",include.data=TRUE) |>
   ggplot(aes(x=week,y=reports,group=.id,color=.id=="data")) +
   geom_line() + guides(color="none")
 
 
-## ----fig.width=5, fig.height=2-----------------------------------------------------------------
+## ----fig.width=5, fig.height=2--------------------------------------------------
 pf <- measSIR |> pfilter(Np=5000)
 min(pf@eff.sample.size)
 
 
-## ----fig.width=7, fig.height=4-----------------------------------------------------------------
+## ----fig.width=7, fig.height=4--------------------------------------------------
 #| out-height: 80%
 plot(pf)
 
 
-## ----pf2---------------------------------------------------------------------------------------
+## ----pf2------------------------------------------------------------------------
 foreach(i=1:10,.combine=c,
   .options.future=list(seed=TRUE)) %dofuture% {
   measSIR |> pfilter(Np=5000)
@@ -80,7 +80,7 @@ pf |> logLik() |> logmeanexp(se=TRUE) -> L_pf
 L_pf
 
 
-## ----init_csv,cache=FALSE----------------------------------------------------------------------
+## ----init_csv,cache=FALSE-------------------------------------------------------
 pf[[1]] |> coef() |> bind_rows() |>
   bind_cols(loglik=L_pf[1],loglik.se=L_pf[2]) |>
   write_csv("measles_params.csv")
@@ -88,7 +88,7 @@ pf[[1]] |> coef() |> bind_rows() |>
 
 
 
-## ----local_search_eval,echo=FALSE--------------------------------------------------------------
+## ----local_search_eval,echo=FALSE-----------------------------------------------
 bake(file="local_search.rds",{
   foreach(i=1:20,.combine=c,
     .options.future=list(seed=482947940)) %dofuture% {
@@ -109,7 +109,7 @@ t_loc <- attr(mifs_local,"system.time")
 ncpu_loc <- attr(mifs_local,"ncpu")
 
 
-## ----local_search_plot,fig.width=6, fig.height=4, dpi=300--------------------------------------
+## ----local_search_plot,fig.width=6, fig.height=4, dpi=300-----------------------
 #| out-height: 80%
 mifs_local |>
   traces() |>
@@ -120,13 +120,41 @@ mifs_local |>
   facet_wrap(~name,scales="free_y")
 
 
-## ----cluster_setup,include=FALSE,purl=TRUE-----------------------------------------------------
+
+
+## ----lik_local_eval,include=FALSE-----------------------------------------------
+bake(file="lik_local.rds",{
+  foreach(mf=mifs_local,.combine=rbind,
+    .options.future=list(seed=900242057)
+  ) %dofuture% {
+    evals <- replicate(10, logLik(pfilter(mf,Np=5000)))
+    ll <- logmeanexp(evals,se=TRUE)
+    mf |> coef() |> bind_rows() |>
+      bind_cols(loglik=ll[1],loglik.se=ll[2])
+  } -> results
+  attr(results,"ncpu") <- nbrOfWorkers()
+  results
+}) -> results
+t_local <- attr(results,"system.time")
+ncpu_local <- attr(results,"ncpu")
+
+
+## ----mle_local------------------------------------------------------------------
+results |> filter(loglik==max(loglik))
+
+
+## ----pairs_local,fig.width=8, fig.height=6, dpi=300-----------------------------
+#| out-height: 80%
+pairs(~loglik+Beta+Eta+Rho,data=results,pch=16)
+
+
+## ----cluster_setup,include=FALSE,purl=TRUE--------------------------------------
 if (file.exists("CLUSTER.R")) {
   source("CLUSTER.R")
 }
 
 
-## ----global_search1----------------------------------------------------------------------------
+## ----global_search1-------------------------------------------------------------
 set.seed(2062379496) 
 runif_design(
   lower=c(Beta=5,Rho=0.2,Eta=0),
@@ -139,7 +167,7 @@ mf1 <- mifs_local[[1]]
 
 
 
-## ----global_search_eval,include=FALSE----------------------------------------------------------
+## ----global_search_eval,include=FALSE-------------------------------------------
 bake(file="global_search.rds",
   dependson=guesses,{
     foreach(guess=iter(guesses,"row"), .combine=rbind,
@@ -164,7 +192,7 @@ t_global <- attr(results,"system.time")
 ncpu_global <- attr(results,"ncpu")
 
 
-## ----cache=FALSE,include=FALSE-----------------------------------------------------------------
+## ----cache=FALSE,include=FALSE--------------------------------------------------
 read_csv("measles_params.csv") |>
   bind_rows(results) |>
   filter(is.finite(loglik)) |>
@@ -172,11 +200,11 @@ read_csv("measles_params.csv") |>
   write_csv("measles_params.csv")
 
 
-## ----mle_global--------------------------------------------------------------------------------
+## ----mle_global-----------------------------------------------------------------
 results |> filter(loglik==max(loglik))
 
 
-## ----pairs_global1,fig.width=8, fig.height=6, dpi=300------------------------------------------
+## ----pairs_global1,fig.width=8, fig.height=6, dpi=300---------------------------
 #| out-height: 80%
 read_csv("measles_params.csv") |>
   filter(loglik>max(loglik)-50) |>
@@ -185,13 +213,13 @@ read_csv("measles_params.csv") |>
   arrange(type) -> all
 
 
-## ----pairs_global11,fig.width=8, fig.height=6, dpi=300-----------------------------------------
+## ----pairs_global11,fig.width=8, fig.height=6, dpi=300--------------------------
 #| out-height: 80%
 pairs(~loglik+Beta+Eta+Rho, data=all, pch=16, cex=0.3,
   col=ifelse(all$type=="guess",grey(0.5),"red"))
 
 
-## ----pairs_global2, fig.width=8, fig.height=6, dpi=300-----------------------------------------
+## ----pairs_global2, fig.width=8, fig.height=6, dpi=300--------------------------
 #| out-height: 80%
 all |>
   filter(type=="result") |>
@@ -204,14 +232,14 @@ all |>
   )
 
 
-## ----eta_profile1a-----------------------------------------------------------------------------
+## ----eta_profile1a--------------------------------------------------------------
 read_csv("measles_params.csv") |>
   filter(loglik>max(loglik)-20,loglik.se<2) |>
   sapply(range) -> box
 box
 
 
-## ----eta_profile1b, fig.width=4, fig.height=2, dpi=300-----------------------------------------
+## ----eta_profile1b, fig.width=4, fig.height=2, dpi=300--------------------------
 freeze(seed=1196696958,
   profile_design(
     Eta=seq(0.01,0.95,length=40),
@@ -221,14 +249,14 @@ freeze(seed=1196696958,
   )) -> guesses
 
 
-## ----fig.width=6, fig.height=4, dpi=300--------------------------------------------------------
+## ----fig.width=6, fig.height=4, dpi=300-----------------------------------------
 #| out-height: 60%
 plot(guesses)
 
 
 
 
-## ----eta_profile2_eval,include=FALSE-----------------------------------------------------------
+## ----eta_profile2_eval,include=FALSE--------------------------------------------
 mf1 <- mifs_local[[1]]
 bake(file="eta_profile.rds",
   dependson=guesses,{
@@ -251,7 +279,7 @@ t_eta <- attr(results,"system.time")
 ncpu_eta <- attr(results,"ncpu")
 
 
-## ----eta_profile_database, echo=FALSE, cache=FALSE---------------------------------------------
+## ----eta_profile_database, echo=FALSE, cache=FALSE------------------------------
 read_csv("measles_params.csv") |>
   bind_rows(results) |>
   filter(is.finite(loglik)) |>
@@ -259,7 +287,7 @@ read_csv("measles_params.csv") |>
   write_csv("measles_params.csv")
 
 
-## ----eta_profile_pairs, fig.width=8, fig.height=6, dpi=300-------------------------------------
+## ----eta_profile_pairs, fig.width=8, fig.height=6, dpi=300----------------------
 #| out-height: 70%
 read_csv("measles_params.csv") |> 
   filter(loglik>max(loglik)-10) -> all
@@ -267,7 +295,7 @@ read_csv("measles_params.csv") |>
 pairs(~loglik+Beta+Eta+Rho,data=all,pch=16)
 
 
-## ----eta_profile_plot2, fig.width=6, fig.height=5, dpi=300-------------------------------------
+## ----eta_profile_plot2, fig.width=6, fig.height=5, dpi=300----------------------
 #| out-height: 80%
 results |>
   filter(is.finite(loglik)) |>
@@ -279,7 +307,7 @@ results |>
   geom_point() + xlab(expression(eta))
 
 
-## ----eta_profile_plot3, fig.width=6, fig.height=4, dpi=300-------------------------------------
+## ----eta_profile_plot3, fig.width=6, fig.height=4, dpi=300----------------------
 #| out-height: 80%
 maxloglik <- max(results$loglik,na.rm=TRUE)
 ci.cutoff <- maxloglik-0.5*qchisq(df=1,p=0.95)
@@ -295,14 +323,14 @@ results |>
   lims(y=maxloglik-c(5,0))
 
 
-## ----eta_ci------------------------------------------------------------------------------------
+## ----eta_ci---------------------------------------------------------------------
 results |>
   filter(is.finite(loglik)) |>
   filter(loglik>max(loglik)-0.5*qchisq(df=1,p=0.95)) |> 
   summarize(min=min(Eta),max=max(Eta)) -> Eta_ci
 
 
-## ----rho_profile1------------------------------------------------------------------------------
+## ----rho_profile1---------------------------------------------------------------
 read_csv("measles_params.csv") |>
   group_by(cut=round(Rho,2)) |>
   filter(rank(-loglik)<=10) |>
@@ -313,7 +341,7 @@ read_csv("measles_params.csv") |>
 
 
 
-## ----rho_profile_eval,include=FALSE------------------------------------------------------------
+## ----rho_profile_eval,include=FALSE---------------------------------------------
 mf1 <- mifs_local[[1]]
 bake(file="rho_profile.rds",
   dependson=guesses,{
@@ -339,7 +367,7 @@ t_rho <- attr(results,"system.time")
 ncpu_rho <- attr(results,"ncpu")
 
 
-## ----include=FALSE,cache=FALSE-----------------------------------------------------------------
+## ----include=FALSE,cache=FALSE--------------------------------------------------
 read_csv("measles_params.csv") |>
   bind_rows(results) |>
   filter(is.finite(loglik)) |>
@@ -347,14 +375,14 @@ read_csv("measles_params.csv") |>
   write_csv("measles_params.csv")
 
 
-## ----prof-rho1, echo=FALSE, fig.width=8, fig.height=6, dpi=300---------------------------------
+## ----prof-rho1, echo=FALSE, fig.width=8, fig.height=6, dpi=300------------------
 #| out-height: 80%
 results |> filter(is.finite(loglik)) -> results
 
 pairs(~loglik+Beta+Eta+Rho,data=results,pch=16)
 
 
-## ----echo=FALSE, fig.width=6, fig.height=4, dpi=300--------------------------------------------
+## ----echo=FALSE, fig.width=6, fig.height=4, dpi=300-----------------------------
 #| out-height: 80%
 results |>
   filter(loglik>max(loglik)-10,loglik.se<1) |>
@@ -369,13 +397,13 @@ results |>
   )
 
 
-## ----rho_ci------------------------------------------------------------------------------------
+## ----rho_ci---------------------------------------------------------------------
 results |>
   filter(loglik>max(loglik)-0.5*qchisq(df=1,p=0.95)) |>
   summarize(min=min(Rho),max=max(Rho)) -> rho_ci
 
 
-## ----------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------
 read_csv("measles_params.csv") |>
   filter(loglik == max(loglik)) |>
   select(-loglik, -loglik.se) -> best.params
@@ -387,7 +415,7 @@ measSIR |>
   ) -> sims
 
 
-## ----viz-predictions, fig.height=4, fig.width=6, dpi=300---------------------------------------
+## ----viz-predictions, fig.height=4, fig.width=6, dpi=300------------------------
 #| out-height: 80%
 sims |>
   mutate(data=.id=="data") |>
